b235655ec7b8d5a26afb657d92df11fb95534f9e
Bruno Haveroth <brun00haveroth@gmail.com>
2016-08-30 14:33:25 -0300

    adjusts in component

--- app/components/ember-pagination.js
+++ app/components/ember-pagination.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement: function() {
+  didReceiveAttrs: function() {
     this.renderPagination();
   },
 
+  remakePagination: Ember.observer('total', function() {
+    this.resetPagesSelected();
+    this.renderPagination();
+  }),
+
   renderPagination: function() {
-    var total = this.get('total'),
+    var total = this.get('total') > 0 ? this.get('total'): 1,
         perPage = this.get('perPage'),
         limit = this.get('limit');
 
@@ -14,11 +19,12 @@ export default Ember.Component.extend({
     var arrayPages = Ember.A();
 
     for (var i = 0; i < pages; i++) {
-      arrayPages.push(i+1);
+      arrayPages.push({ pageNumber: i+1, selected: false });
     }
 
     this.set('arrayPagesFull', arrayPages);
-    this.set('arrayPages', arrayPages.slice(0, limit));
+    this.set('arrayPages', Ember.A(arrayPages.slice(0, limit)));
+    Ember.set(this.get('arrayPages.firstObject'), 'selected', true);
     this.updatePrevNext();
   },
 
@@ -26,36 +32,52 @@ export default Ember.Component.extend({
     var min = this.get('arrayPages.firstObject'),
       max = this.get('arrayPages.lastObject'),
       arrayPagesFull = this.get('arrayPagesFull');
+    this.set('havePrev', (min.pageNumber > 1 ? true:false));
+    this.set('haveNext', (max.pageNumber < arrayPagesFull.length ? true:false));
+  },
 
-    this.set('havePrev', (min > 1 ? true:false));
-    this.set('haveNext', (max < arrayPagesFull.length ? true:false));
+  resetPagesSelected: function(element) {
+    var arrayPages = this.get('arrayPages');
+    arrayPages.forEach(function(page) {
+      Ember.set(page, 'selected', false);
+    });
+  },
+
+  selectNextPrevPage: function(direction) {
+    var arrayPages = this.get('arrayPages');
+    var page = direction === 'next' ? arrayPages.get('firstObject') : arrayPages.get('lastObject');
+    this.send('loadNewPage', page);
   },
 
   actions: {
     loadNewPage: function(page) {
-      this.sendAction('loadAction', page);
+      this.sendAction('loadAction', page.pageNumber);
+      this.resetPagesSelected();
+      Ember.set(page, 'selected', true);
     },
 
     next: function() {
-      var skip = this.get('arrayPages.lastObject');
+      var skip = this.get('arrayPages.lastObject.pageNumber');
       var arrayPagesFull = this.get('arrayPagesFull');
 
       if (skip < arrayPagesFull.length) {
         this.set('haveNext', true);
-        this.set('arrayPages', arrayPagesFull.slice(skip, skip + this.get('limit')));
+        this.set('arrayPages', Ember.A(arrayPagesFull.slice(skip, skip + this.get('limit'))));
       }
       this.updatePrevNext();
+      this.selectNextPrevPage('next');
     },
 
     prev: function() {
-      var skip = this.get('arrayPages.firstObject');
+      var skip = this.get('arrayPages.firstObject.pageNumber');
       skip--;
       var arrayPagesFull = this.get('arrayPagesFull');
       if (skip !== 0) {
         this.set('havePrev', true);
-        this.set('arrayPages', arrayPagesFull.slice(skip - this.get('limit'), skip));
+        this.set('arrayPages', Ember.A(arrayPagesFull.slice(skip - this.get('limit'), skip)));
       }
       this.updatePrevNext();
+      this.selectNextPrevPage('prev');
     }
   }
 });
